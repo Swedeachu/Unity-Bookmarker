427b28e8c621de157a605eef97973fba70032d85
swim <camden_w_brown@outlook.com>
2025-09-09 14:56:44 -0800

    Add project files.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+  void Start()
+  {
+    Debug.Log("Started!");
+  }
+
+  void Update()
+  {
+
+  }
+
+}
